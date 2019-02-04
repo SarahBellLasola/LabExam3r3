0a352dc9461238d89e0a98814c2794ce60a4883e
Sarah Bell Lasola <45348917+SarahBellLasola@users.noreply.github.com>
2019-02-04 16:24:38 +0800

    Add files via upload

---
+++ Ajax.js
@@ -0,0 +1,17 @@
+Let xhttp=new XMLHttpRequest();
+function XMLHttpRequest() {
+
+	for onreadystagechange==4&&status==200 {
+		if (this.onreadystatechange==4&&this.status==200) {
+
+			document.getElementById('#btn'){
+				this.ResponseText();
+			}
+		}
+	}
+	xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos",true);
+	xhttp.send();
+
+}
+
+xhttp.onreadystatechange=
